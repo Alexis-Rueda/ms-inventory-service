886dc60ec9b33ad9af633ae06487b4826a6974d1
alexis.rueda <alexisrueda34@gmail.com>
2026-06-13 01:50:46 -0600

    Mejorar formato y agregar registro de puerto en el método getAllInventory del controlador de inventario

--- src/main/java/com/ecommerce/inventory_service/controller/InventoryController.java
+++ src/main/java/com/ecommerce/inventory_service/controller/InventoryController.java
@@ -3,6 +3,7 @@ package com.ecommerce.inventory_service.controller;
 import com.ecommerce.inventory_service.dto.InventoryRequest;
 import com.ecommerce.inventory_service.dto.InventoryResponse;
 import com.ecommerce.inventory_service.service.InventoryService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,41 +17,42 @@ import java.util.List;
 public class InventoryController {
     private final InventoryService inventoryService;
 
-
     @GetMapping("/{sku}")
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku") String sku, @RequestParam("quantity") Integer quantity) {
+    public boolean isInStock(@PathVariable("sku") String sku,@RequestParam("quantity") Integer quantity){
         return inventoryService.isInStock(sku, quantity);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public InventoryResponse createInventory(@Valid @RequestBody InventoryRequest inventoryRequest) {
+    public InventoryResponse createInventory(@Valid @RequestBody InventoryRequest inventoryRequest){
         return inventoryService.createInventory(inventoryRequest);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<InventoryResponse> getAllInventory() {
+    public List<InventoryResponse> getAllInventory(HttpServletRequest request)
+    {
+        System.out.println("✅ Petición atendida desde el puerto: " + request.getServerPort());
         return inventoryService.getAllInventory();
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public InventoryResponse updateInventory(@PathVariable Long id, @Valid @RequestBody InventoryRequest inventoryRequest) {
+    public InventoryResponse updateInventory( @PathVariable Long id,@Valid @RequestBody InventoryRequest inventoryRequest){
         return inventoryService.updateInventory(id, inventoryRequest);
     }
 
     @PutMapping("/reduce/{sku}")
     @ResponseStatus(HttpStatus.OK)
-    public String reduceStock(@PathVariable String sku, @RequestParam Integer quantity) {
+    public String reduceStock( @PathVariable String sku, @RequestParam Integer quantity){
         inventoryService.reduceStock(sku, quantity);
         return "Stock reducido exitosamente";
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteInventory(@PathVariable Long id) {
+    public void deleteInventory(@PathVariable Long id){
         inventoryService.deleteInventory(id);
     }
 }
